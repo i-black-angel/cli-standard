f4a5b9f0ad8b094f56a32a59a3a96f7f29b383d1
Shusheng Shao <iblackangel@163.com>
2015-07-09 11:18:29 +0800

    remove all temp files and add autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+DIE=0
+
+(autoconf --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have autoconf installed to compile cli-standard."
+    echo "Download the appropriate package for your distribution,"
+    echo "or see http://www.gnu.org/software/autoconf"
+    DIE=1
+}
+
+(automake --version) < /dev/null > /dev/null 2>&1 || {
+    echo
+    echo "You must have automake installed to compile cli-standard."
+    echo "Download the appropriate package for your distribution,"
+    echo "or see http://www.gnu.org/software/automake"
+    DIE=1
+}
+
+if [ "$DIE" -eq 1 ]; then
+    exit 1
+fi
+
+# Replaced by autoreconf below
+autoreconf -if
